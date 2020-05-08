b5d4d864b50bc9b311256ccc3d1da75ade943aed
JHades <jhades.dev@gmail.com>
2020-05-08 12:09:06 +0200

    Angular Router In Depth

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,22 +1,40 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 import {AuthStore} from "./auth.store";
 import {Observable} from "rxjs";
-import {filter} from "rxjs/operators";
+import {filter, map} from "rxjs/operators";
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate, CanActivateChild {
 
-  constructor(private auth: AuthStore) {
+  constructor(private auth: AuthStore, private router: Router) {
 
   }
 
-  canActivate(snapshot: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.auth.isLoggedIn$;
+  canActivate(snapshot: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this.checkIfAuthenticated();
   }
 
-  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.auth.isLoggedIn$;
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this.checkIfAuthenticated();
+  }
+
+  private checkIfAuthenticated() {
+    return this.auth.isLoggedIn$
+      .pipe(map(loggedIn => {
+          if (!loggedIn) {
+            return this.router.parseUrl('/login');
+          }
+          else return true;
+        })
+      );
   }
 
 }
