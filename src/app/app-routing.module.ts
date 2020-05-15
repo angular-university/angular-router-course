3b96acf7a88aedf0974f89ec44cf1a21de97cafe
Your Name <you@example.com>
2020-05-15 14:47:45 +0200

    Angular Router Course

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {AboutComponent} from './about/about.component';
 
 
 const routes: Routes = [
-
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    }
 ];
 
 @NgModule({
   imports: [
-
+      RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
