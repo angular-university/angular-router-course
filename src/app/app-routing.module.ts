603e7d03392349850c1aae803debc7c90acdd674
Your Name <you@example.com>
2020-05-22 14:27:32 +0200

    Angular Router Course

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
     {
+      path: "courses",
+      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    },
+    {
         path: "login",
         component: LoginComponent
     },
