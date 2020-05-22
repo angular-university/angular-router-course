abf6cbbe080b29056bd88073eabf76c759bfb517
Your Name <you@example.com>
2020-05-22 14:53:24 +0200

    Angular Router Course

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Course} from '../model/course';
 
 
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+    course: Course;
+
+    couponCode: string;
 
 
-  constructor() {
+    constructor() {
 
 
-  }
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
 
-  }
+    }
 
 
 }
