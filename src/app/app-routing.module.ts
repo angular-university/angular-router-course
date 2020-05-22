16341f6e7b84f54066bad59dbc8692b056be091a
Your Name <you@example.com>
2020-05-22 15:54:31 +0200

    Angular Router Course

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AboutComponent} from './about/about.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+    {
+      path: "",
+      redirectTo: "/courses",
+        pathMatch: "full"
+    },
     {
       path: "courses",
       loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
@@ -16,7 +22,12 @@ const routes: Routes = [
     {
         path: "about",
         component: AboutComponent
+    },
+    {
+        path: "**",
+        component: PageNotFoundComponent
     }
+
 ];
 
 @NgModule({
