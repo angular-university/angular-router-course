930b1c5e5009c6219d6e3ba3a9dd564bbf747a75
Chidume Nnamdi <kurtwanger40@gmail.com>
2024-11-26 15:00:59 +0100

    Upgrade Angular 19

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -4,9 +4,10 @@ import {LoadingService} from './loading.service';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
 
 @Component({
-  selector: 'loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+    selector: 'loading',
+    templateUrl: './loading.component.html',
+    styleUrls: ['./loading.component.css'],
+    standalone: false
 })
 export class LoadingComponent implements OnInit {
 
